2e2ee8cbc842145816e5017e821386a05d6b8393
seongholee4 <seongholee4@gmail.com>
2023-04-28 02:52:20 +0900

    stars edit

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -2,11 +2,32 @@ import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
+function randomInSphere(numPoints, radius) {
+  const points = new Float32Array(numPoints * 3);
+  
+  for (let i = 0; i < numPoints; i++) {
+    let x, y, z, sqrDistance;
+    
+    do {
+      x = Math.random() * 2 - 1; // Random number between -1 and 1
+      y = Math.random() * 2 - 1;
+      z = Math.random() * 2 - 1;
+      sqrDistance = x * x + y * y + z * z;
+    } while (sqrDistance >= 1); // Keep generating points until one is inside the unit sphere
 
+    // Scale the point to the desired radius and store it in the array
+    const scaleFactor = radius * Math.cbrt(Math.random());
+    points[i * 3] = x * scaleFactor;
+    points[i * 3 + 1] = y * scaleFactor;
+    points[i * 3 + 2] = z * scaleFactor;
+  }
+  
+  return points;
+}
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
-
+  // const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  const sphere = randomInSphere(5000, 1.2);
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
